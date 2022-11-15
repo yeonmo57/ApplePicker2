fb9c0a0bf0fa862c0976655fcd448991a1cbc75b
moorin <onpa@naver.com>
2022-11-15 18:41:50 +0900

    daily 불필요한 주석 제거

--- app/src/main/java/todo/swu/applepicker/FragmentDaily.java
+++ app/src/main/java/todo/swu/applepicker/FragmentDaily.java
@@ -48,16 +48,7 @@ import java.util.Map;
 public class FragmentDaily extends Fragment {
     ImageButton iButton_calendar;
     TextView tv_date;
-    /*
-    EditText edit_dDay;
-    EditText edit_comment;
-    EditText edit_total_time;
-    EditText edit_subject;
-    EditText edit_part;
-     */
     EditText edit_memo;
-
-    //ImageButton iButton_task_add;
 
     ImageButton iButton_memo_add;
 
@@ -70,8 +61,6 @@ public class FragmentDaily extends Fragment {
 
     ArrayList<MemoItem> memoItemList;
 
-    //ListView memolistview;
-    //TaskAdapter taskAdapter;
     MemoAdapter memoAdapter;
 
 
@@ -80,8 +69,6 @@ public class FragmentDaily extends Fragment {
                              Bundle savedInstanceState) {
 
         View myView = inflater.inflate(R.layout.fragment_daily, container, false);
-
-
 
         //이 버튼을 클릭하면 달력이 호출됨.
         iButton_calendar = (ImageButton) myView.findViewById(R.id.iButton_calendar);
@@ -93,37 +80,9 @@ public class FragmentDaily extends Fragment {
         //Access a Firestore
         db = FirebaseFirestore.getInstance();
 
-        db.collection("isDataPassing")
-                .document("isDataPassing") //선택한 날짜에 해당하는 데이터 유무 확인
-                .get()
-                .addOnSuccessListener(snapShotData -> {
-                    if (snapShotData.exists()) {//선택한 날짜에 저장된 데이터가 있는 경우 해당 data 갖고와서 화면에 뿌려줌.
-                        //Log.e("선택한 날짜에 저장된 데이터가 있는 경우 해당 data 갖고와서 화면에 뿌려줌.", dateToday);
-
-                        String flag = (String) snapShotData.getData().get("flag");
-                        Log.e("flag 값: ", flag);
-
-                        if(flag.equals("True")) {
-
-
-                            Log.e("데일리 프레그먼트", "if문 안");
-                        }
-                    }
-                }).addOnFailureListener(e -> e.printStackTrace());
-
-
-        //Task RecyclerView에 표시할 데이터 리스트 생성.
-        /*
-        taskItemList = new ArrayList<TaskItem>();
-        //RecyclerView에 LinearLayoutManager 객체 지정, 어댑터 연결
-        taskRecyclerView = (RecyclerView) myView.findViewById(R.id.recyclerView_task);
-        taskRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-        taskAdapter = new TaskAdapter(taskItemList);
-        taskRecyclerView.setAdapter(taskAdapter);
-        */
-
         //Memo RecyclerView에 표시할 데이터 리스트 생성.
         memoItemList = new ArrayList<MemoItem>();
+
         //RecyclerView에 LinearLayoutManager 객체 지정, 어댑터 연결.
         RecyclerView memoRecyclerView = (RecyclerView) myView.findViewById(R.id.recyclerView_memo);
         memoRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
@@ -138,97 +97,6 @@ public class FragmentDaily extends Fragment {
             dateFragment.show(getActivity().getSupportFragmentManager(), "dateFragment");
         });
 
-        /*
-        edit_dDay.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-            }
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                String field = "dDay";
-                updateDB(field, edit_dDay);
-            }
-            @Override
-            public void afterTextChanged(Editable editable) {
-                edit_dDay.setTextColor(getActivity().getResources().getColor(R.color.green_text));
-                edit_dDay.setBackground(null);
-            }
-        });
-
-         */
-
-        /*
-        edit_comment.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-            }
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                String field = "comment";
-                updateDB(field, edit_comment);
-            }
-            @Override
-            public void afterTextChanged(Editable editable) {
-                edit_comment.setTextColor(getActivity().getResources().getColor(R.color.green_text));
-                edit_comment.setBackground(null);
-            }
-        });
-
-         */
-
-        /*
-        edit_total_time.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-            }
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                String field = "totalTime";
-                updateDB(field, edit_total_time);
-            }
-            @SuppressLint("LongLogTag")
-            @Override
-            public void afterTextChanged(Editable editable) {
-                Log.e("edit_total_time의 afterTextChanged 호출됨.", currentDate);
-                edit_total_time.setTextColor(getActivity().getResources().getColor(R.color.green_text));
-                edit_total_time.setBackground(null);
-            }
-        });
-
-         */
-
-        /*
-        edit_subject.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-            }
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-            }
-            @Override
-            public void afterTextChanged(Editable editable) {
-                edit_subject.setBackground(null);
-            }
-        });
-
-         */
-
-        /*
-        edit_part.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-            }
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-            }
-            @Override
-            public void afterTextChanged(Editable editable) {
-                edit_part.setBackground(null);
-            }
-        });
-
-         */
-
         edit_memo.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
@@ -241,22 +109,6 @@ public class FragmentDaily extends Fragment {
                 edit_memo.setBackground(null);
             }
         });
-
-/*
-        iButton_task_add.setOnClickListener(v -> {
-            Timestamp timestamp = Timestamp.now();
-            taskItemList.add(new TaskItem(timestamp, edit_subject.getText().toString(),
-                edit_part.getText().toString(), false));
-
-            addTaskDB(timestamp);
-            EventChangeListener();
-            Log.e(TAG, "DB에 Task 저장됨 => ");
-
-            edit_subject.setText(null);
-            edit_part.setText(null);
-        });
-
- */
 
         iButton_memo_add.setOnClickListener(v -> {
             // 리사이클러뷰에 SimpleTextAdapter 객체 지정.
@@ -275,15 +127,11 @@ public class FragmentDaily extends Fragment {
             Log.e(TAG, "DB에 Task 저장됨 => ");
 
             edit_memo.setText(null);
-            //Log.e(TAG, "setText 널");
         });
-
-        // Inflate the layout for this fragment
 
         return myView;
 
     } //onCreateView End.
-
 
     //오늘날짜에 해당하는 데이터 불러와 화면에 보여줌.
     @SuppressLint("LongLogTag")
@@ -304,50 +152,31 @@ public class FragmentDaily extends Fragment {
         // 불러오기
         //오늘 날짜에 저장된 데이터 불러와서 EditText에 setText()하기
         db.collection("daily")
-            .document(dateToday)//선택한 날짜에 해당하는 데이터 유무 확인
-            .get()
-            .addOnSuccessListener(snapShotData -> {
-                if (snapShotData.exists()) {//선택한 날짜에 저장된 데이터가 있는 경우 해당 data 갖고와서 화면에 뿌려줌.
-                    Log.e("선택한 날짜에 저장된 데이터가 있는 경우 해당 data 갖고와서 화면에 뿌려줌.", dateToday);
+                .document(dateToday)//선택한 날짜에 해당하는 데이터 유무 확인
+                .get()
+                .addOnSuccessListener(snapShotData -> {
+                    if (snapShotData.exists()) {//선택한 날짜에 저장된 데이터가 있는 경우 해당 data 갖고와서 화면에 뿌려줌.
+                        Log.e("선택한 날짜에 저장된 데이터가 있는 경우 해당 data 갖고와서 화면에 뿌려줌.", dateToday);
 
-                    String memoItem = (String) snapShotData.getData().get("memoItem");
+                        String memoItem = (String) snapShotData.getData().get("memoItem");
 
 
-                    // 메모 에디터 텍스트
-                    edit_memo.setText(dateToday);
+                        // 메모 에디터 텍스트
+                        edit_memo.setText(dateToday);
 
-                } else {//선택한 날짜에 해당하는 데이터가 없는 경우
-                    //새로 만들어서 DB에 추가함
-                    Log.e("선택한 날짜에 해당하는 데이터가 없는 경우", dateToday);
-                    db.collection("daily").document(dateToday)
-                        .set(dailyMap)
-                        .addOnSuccessListener(documentReference -> {
-                            Log.e(TAG, "DocumentSnapshot added with ID: ");
-                        }).addOnFailureListener(e -> {
+                    } else {//선택한 날짜에 해당하는 데이터가 없는 경우
+                        //새로 만들어서 DB에 추가함
+                        Log.e("선택한 날짜에 해당하는 데이터가 없는 경우", dateToday);
+                        db.collection("daily").document(dateToday)
+                                .set(dailyMap)
+                                .addOnSuccessListener(documentReference -> {
+                                    Log.e(TAG, "DocumentSnapshot added with ID: ");
+                                }).addOnFailureListener(e -> {
                             Log.e(TAG, "Error adding document", e);
                         });
-                }
-            }).addOnFailureListener(e -> e.printStackTrace());
-
-        /*
-        //오늘 날짜에 저장된 데이터 불러와 RecyclerView 2개에 setText()하기
-        Query query = db.collection("daily").document(dateToday)
-            .collection("task").orderBy("createdAt");
-        query.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                if (task.isSuccessful()) {
-                    for (QueryDocumentSnapshot doc : task.getResult()) {
-                        TaskItem obj = doc.toObject(TaskItem.class);
-                        taskItemList.add(obj);
                     }
-                    taskAdapter.notifyDataSetChanged();
-                } else {
-                    Log.e(TAG, "Recyclerview 초기화 else 부분");
-                }
-            }
-        });
-         */
+                }).addOnFailureListener(e -> e.printStackTrace());
+
 
         //오늘 날짜에 저장된 데이터 불러와 RecyclerView 2개에 setText()하기
         Query query = db.collection("daily").document(dateToday)
@@ -366,11 +195,7 @@ public class FragmentDaily extends Fragment {
                 }
             }
         });
-
-
     }
-
-
 
     @SuppressLint("LongLogTag")
     public void processDatePickerResult(String year, String month, String day, String day_of_week, String datePicked) {
@@ -412,29 +237,6 @@ public class FragmentDaily extends Fragment {
                 }
             }).addOnFailureListener(e -> e.printStackTrace());
 
-        /*
-        //Task
-        taskItemList = new ArrayList<TaskItem>();
-        db.collection("daily").document(datePicked)
-            .collection("task").orderBy("createdAt")
-            .addSnapshotListener(new EventListener<QuerySnapshot>() {
-                @Override
-                public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
-                    if (error != null) {
-                        Log.e("Firestore Error", error.getMessage());
-                    }
-                    for (DocumentSnapshot dc : value.getDocuments()) {
-                        TaskItem obj = dc.toObject(TaskItem.class);
-                        taskItemList.add(obj);
-                    }
-                    //taskAdapter.notifyDataSetChanged();
-                    taskAdapter = new TaskAdapter(taskItemList);
-                    taskRecyclerView.setAdapter(taskAdapter);
-                }
-            });
-
-         */
-
         //Memo
         memoItemList = new ArrayList<MemoItem>();
         db.collection("daily").document(datePicked)
@@ -471,29 +273,6 @@ public class FragmentDaily extends Fragment {
                 Log.e(TAG, "Error updating document", e);
             });
     }
-
-    /*
-    //RecyclerView Task에 입력&변경사항 생길때마다 DB 업데이트함.
-    @SuppressLint("LongLogTag")
-    public void addTaskDB(Timestamp timestamp) {
-        Map<String, Object> taskMap = new HahMap<>();
-        taskMap.put("createdAt", timestamp);
-
-        taskMap.put("subject", edit_subject.getText().toString());
-        taskMap.put("part", edit_part.getText().toString());
-        taskMap.put("achievement", false);
-
-        //task 업데이트.
-        db.collection("daily").document(currentDate)
-            .collection("task")
-            .document(timestamp.toString()).set(taskMap)
-            .addOnSuccessListener(documentReference -> {
-                Log.e(TAG, "DocumentSnapshot added with ID: ");
-            }).addOnFailureListener(e -> {
-                Log.e(TAG, "Error adding document", e);
-            });
-    }
- */
 
     //RecyclerView Memo에 입력&변경사항 생길때마다 DB 업데이트함.
     @SuppressLint("LongLogTag")
@@ -549,30 +328,6 @@ public class FragmentDaily extends Fragment {
         }
         return month + "/" + day + "(" + day_of_week + ")";
     }
-
-/*
-    public void EventChangeListener() {
-        taskItemList = new ArrayList<TaskItem>();
-        db.collection("daily").document(currentDate)
-            .collection("task").orderBy("createdAt")
-            .addSnapshotListener(new EventListener<QuerySnapshot>() {
-                @Override
-                public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
-                    if (error != null) {
-                        Log.e("Firestore Error", error.getMessage());
-                    }
-                    for (DocumentChange dc : value.getDocumentChanges()) {
-                        if (dc.getType() == DocumentChange.Type.ADDED) {
-                            TaskItem obj = dc.getDocument().toObject(TaskItem.class);
-                            taskItemList.add(obj);
-                        }
-                        taskAdapter.notifyDataSetChanged();
-                    }
-                }
-            });
-    }
-
- */
 
     //Memo로 수정
     public void EventChangeListener() {
